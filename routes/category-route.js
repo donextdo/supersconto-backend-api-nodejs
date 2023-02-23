bced62d521f9981d3055a108f6e406aa3882f450
sudheerasrinath <sudheerasrinath99@gmail.com>
2023-02-23 14:06:23 +0530

    corrections for category

--- routes/category-route.js
+++ routes/category-route.js
@@ -4,5 +4,10 @@ const categoryController = require('../controllers/category_controller');
 
 router.post('/main-categories', categoryController.createMainCategory);
 router.post('/sub-categories', categoryController.createSubCategory);
+router.get('/categories', categoryController.getAllMainCategories);
+router.get('/categories/:id', categoryController.getMainCategoryById);
+router.put('/categories/:id', categoryController.updateMainCategory);
+router.delete('/categories/:id', categoryController.deleteMainCategory);
+
 
 module.exports = router;
