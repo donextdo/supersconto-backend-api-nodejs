775a1482e9f44f7f4a5a254d408cea63839f313a
kalanaw2 <manjitharock@gmail.com>
2023-05-19 19:07:06 +0530

    fix image preview

--- models/catalog_page_item.js
+++ models/catalog_page_item.js
@@ -81,7 +81,83 @@ const CatelogBookPageItemSchema = new mongoose.Schema({
     coordinates: {
         type: mongoose.Schema.Types.Mixed,
         default: {}
-    }
+    },
+      brand: {
+        type: String,
+      },
+     
+      front: {
+        type: String,
+      },
+      side: {
+        type: String,
+      },
+      back: {
+        type: String,
+      },
+      // category: [
+      //   {
+      //     type: mongoose.Schema.Types.ObjectId,
+      //     ref: "Category",
+      //   },
+      // ],
+    
+      isAvailable: {
+        type: Boolean,
+        // required: true,
+      },
+      skuNumber: {
+        type: String,
+        // required: true,
+      },
+      type: {
+        type: String,
+        // required: true,
+      },
+      mfgDate: {
+        type: String,
+        // required: true,
+      },
+      expDate: {
+        type: String,
+        // required: true,
+      },
+      discount: {
+        type: Number,
+      },
+      review: {
+        type: Number,
+      },
+    
+      soldCount: {
+        type: Number,
+      },
+    
+      popularity: {
+        type: Number,
+      },
+    
+      averageRating: {},
+    
+      additionalInformation: [],
+    
+      tags: [],
+    
+      updatedAt: {
+        type: Date,
+        default: null,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      deletedAt: {
+        type: Date,
+        default: null,
+      },
+      life: {
+        type: String,
+      },
 
 }, {timestamps: true})
 
